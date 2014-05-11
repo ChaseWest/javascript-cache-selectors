7bc8ee14f7ed36110c460b453a5e0fb2f9d39a35
Chase West <chaserit@gmail.com>
2014-05-10 20:26:18 -0700

    updated cache js

--- cache.js
+++ cache.js
@@ -1,5 +1,28 @@
-;(function(window, document, undefined){
+var jsc = (function(window, document, undefined){
 
+    _cache = {};
 
+    function cache(selector, parent, all){
+        var key = _generateKey();
+        all = !!all;
+        parent = !!parent ? parent : document;
+        _cache[key] = all ? parent.querySelectorAll(selector) : parent.querySelector(selector);
+
+        return key;
+
+    }
+
+    function get(key){
+        return _cache[key];
+    }
+
+    function _generateKey(){
+        return Math.random().toString(36).slice(2);
+    }
+
+    return{
+        cache: cache,
+        get: get
+    }; 
 
 })(window, document);
